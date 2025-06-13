4f4e5fc6b49c063f9f3b7151e35651ae2d54f9d3
FilippovAnton <71548174+FilippovAnton@users.noreply.github.com>
2025-06-13 17:55:16 +0300

    Update 1.py
    the test worked, now the code itself

--- 1.py
+++ 1.py
@@ -1,11 +1,45 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-def my_function(x):
-    return x**2
+@st.cache_data
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("Пример вызова функции")
+def load_data(path):
+    df = pd.read_csv(path, parse_dates=['Date'])  
+    return df
 
-num = st.number_input("Введите число", value=2)
-if st.button("Вычислить квадрат"):
-    result = my_function(num)
-    st.write(f"Результат: {result}")
+def create_plot(df_new, name='bijiy'):
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+    for color, time in zip(['black', 'red', 'brown', 'green', 'blue'], [30, 60, 180, 240, 300]):
+        df_loc = df_new[(df_new['Name'] == name) & (df_new['Turn'] == 0) & (df_new['Time'] == time)].copy()
+        df_loc['SumDif'] = df_loc['RatingDif'].cumsum()
+        x = pd.to_datetime(df_loc['Date'])
+        y = df_loc['SumDif']
+        ax.plot(x, y, color=color, label=str(time))
+        if not df_loc.empty:
+            ax.text(x.iloc[-1], y.iloc[-1], str(len(df_loc)), color=color,
+                    fontsize=12, verticalalignment='bottom', horizontalalignment='left')
+
+    ax.set_xlabel('Время')
+    ax.set_ylabel('Изменение рейтинга')
+    ax.set_title('Изменение рейтинга в нетурнирных партиях')
+    ax.grid(True)
+    ax.legend()
+
+    return fig
+
+def main():
+    st.title("Graph")
+    df = load_data("https://drive.google.com/file/d/17sn61A1ntXtVJmr9aINGB8lOPBVU0H-7/view?usp=sharing")
+    name = st.number_input("Enter the player's nickname", value=2)
+    if st.button("Get player statistics"):
+        fig = create_plot(df, name)
+        st.pyplot(fig)  # отображаем график в приложении
+
+if __name__ == "__main__":
+    main()
