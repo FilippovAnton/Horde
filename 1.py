75c35cb27a70a1ef0abded0a001692b6ca16b422
FilippovAnton <71548174+FilippovAnton@users.noreply.github.com>
2025-06-13 19:18:15 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
+import gdown
 
 @st.cache_data
-def load_data(path):
-    df = pd.read_csv(path) #, index_col=0, parse_dates=['Date']) 
+def load_data(url):
+    output = 'file.csv'
+    gdown.download(url, output, quiet=False)
+    df = pd.read_csv(output, index_col=0, parse_dates=['Date'])
     return df
 
 def create_plot(df_new, name='bijiy'):
@@ -31,9 +34,10 @@ def create_plot(df_new, name='bijiy'):
 
 def main():
     st.title("Graph")
-    url = "https://drive.google.com/uc?export=download&id=17sn61A1ntXtVJmr9aINGB8lOPBVU0H-7"
-    df = load_data(url)
+    url = 'https://drive.google.com/uc?id=1SxWKiFPIYGfHYb4NdPlegQbnIhD9K5hA'
+    df = load_data(url) 
     name = st.text_input("Enter the player's nickname", value="bijiy")
+    
     if st.button("Get player statistics"):
         fig = create_plot(df, name)
         st.pyplot(fig)
